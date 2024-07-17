08e6a8830443b4baf74908c0d65a55f1f1868569
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-07-17 21:47:51 +0300

    Fix the weighted selection for enemy groups

--- Mod/SpawnManager.cs
+++ Mod/SpawnManager.cs
@@ -1,9 +1,60 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 namespace WalkerSim
 {
     internal class SpawnManager
     {
+        static int _lastClassId = -1;
+
+        // NOTE: The original EntityGroups.GetRandomFromGroupList is not correct.
+        // Illustration for 100 iterations:
+        // Original:
+        //  Selection 0 occurred 79 times.
+        //  Selection -1 occurred 10 times.
+        //  Selection 1 occurred 5 times.
+        //  Selection 2 occurred 6 times.
+        // Custom:
+        //  Selection 0 occurred 86 times.
+        //  Selection 1 occurred 5 times.
+        //  Selection 2 occurred 9 times.
+        // -1 should only occur when the group list is empty which it isnt.
+        private static int GetRandomFromGroupList(List<SEntityClassAndProb> grpList, GameRandom random)
+        {
+            var totalWeight = 0.0f;
+            foreach (var entry in grpList)
+            {
+                totalWeight += entry.prob;
+            }
+            var max = random.RandomFloat * totalWeight;
+            var cur = 0.0f;
+            foreach (var entry in grpList)
+            {
+                cur += entry.prob;
+                if (cur > max)
+                {
+                    return entry.entityClassId;
+                }
+            }
+            return -1;
+        }
+
+        private static int GetRandomFromGroup(string _sEntityGroupName, ref int lastClassId, GameRandom random = null)
+        {
+            List<SEntityClassAndProb> grpList = EntityGroups.list[_sEntityGroupName];
+            int num = 0;
+            for (int i = 0; i < 3; i++)
+            {
+                num = GetRandomFromGroupList(grpList, random);
+                if (num != lastClassId)
+                {
+                    lastClassId = num;
+                    break;
+                }
+            }
+            return num;
+        }
+
         static private bool CanSpawnZombie()
         {
             // Check for maximum count.
@@ -14,7 +65,7 @@ namespace WalkerSim
 
             if (alive >= maxAllowed)
             {
-                //Logging.Out("Max zombies reached, alive: {0}, max: {1}", alive, maxAllowed);
+                Logging.Debug("Max zombies reached, alive: {0}, max: {1}", alive, maxAllowed);
                 return false;
             }
 
@@ -86,17 +137,18 @@ namespace WalkerSim
                         continue;
                     }
 
-                    int lastClassId = -1;
-                    int entityClassId = EntityGroups.GetRandomFromGroup(group.entityGroupRefName, ref lastClassId);
-                    if (entityClassId == 0)
+                    if (!EntityGroups.list.ContainsKey(group.entityGroupRefName))
                     {
-                        Logging.Warn("Failed to get entity class id from {0}, returned 0", group.entityGroupRefName);
+                        Logging.Err("Entity group not found: {0}", group.entityGroupRefName);
                         continue;
                     }
 
-                    if (entityClassId != -1)
+                    int lastClassId = _lastClassId;
+
+                    int entityClassId = GetRandomFromGroup(group.entityGroupRefName, ref lastClassId, gameRandom);
+                    if (entityClassId != -1 && entityClassId != 0)
                     {
-                        Logging.Out("Selected entity class id {0} : {1}", entityClassId, group.entityGroupRefName);
+                        Logging.Debug("Selected entity class id {0} : {1}", entityClassId, group.entityGroupRefName);
                         return entityClassId;
                     }
                 }
@@ -129,19 +181,19 @@ namespace WalkerSim
             var chunk = world.GetChunkSync(chunkPosX, chunkPosZ) as Chunk;
             if (chunk == null)
             {
-                Logging.Out("Failed to spawn agent, chunk not loaded at {0}, {1}", chunkPosX, chunkPosZ);
+                Logging.DebugErr("Failed to spawn agent, chunk not loaded at {0}, {1}", chunkPosX, chunkPosZ);
                 return -1;
             }
 
             var terrainHeight = world.GetTerrainHeight(Mathf.FloorToInt(worldPos.x), Mathf.FloorToInt(worldPos.z)) + 1;
-            Logging.Out("Terrain height at {0}, {1} is {2}", worldPos.x, worldPos.z, terrainHeight);
+            Logging.Debug("Terrain height at {0}, {1} is {2}", worldPos.x, worldPos.z, terrainHeight);
 
             // Adjust position height.
             worldPos.y = terrainHeight;
 
             if (!CanSpawnAtPosition(worldPos))
             {
-                Logging.Out("Failed to spawn agent, position not suitable at {0}, {1}, {2}", worldPos.x, worldPos.y, worldPos.z);
+                Logging.Debug("Failed to spawn agent, position not suitable at {0}, {1}, {2}", worldPos.x, worldPos.y, worldPos.z);
                 return -1;
             }
 
@@ -167,7 +219,7 @@ namespace WalkerSim
             }
             else
             {
-                Logging.Out("Using previous entity class id: {0}", entityClassId);
+                Logging.Debug("Using previous entity class id: {0}", entityClassId);
             }
 
             var rot = VectorUtils.ToUnity(agent.Velocity);
@@ -177,9 +229,12 @@ namespace WalkerSim
             var spawnedAgent = EntityFactory.CreateEntity(entityClassId, worldPos, rot) as EntityZombie;
             if (spawnedAgent == null)
             {
-                Logging.Err("Unable to create zombie entity!, Class Id: {0}, Pos: {1}", entityClassId, worldPos);
+                Logging.DebugErr("Unable to create zombie entity!, Class Id: {0}, Pos: {1}", entityClassId, worldPos);
                 return -1;
             }
+
+            // Only update last class id if we successfully spawned the agent.
+            _lastClassId = entityClassId;
 
             spawnedAgent.bIsChunkObserver = true;
             spawnedAgent.IsHordeZombie = true;
@@ -188,7 +243,7 @@ namespace WalkerSim
 
             if (agent.Health != -1)
             {
-                Logging.Out("Using previous health: {0}", agent.Health);
+                Logging.Debug("Using previous health: {0}", agent.Health);
                 spawnedAgent.Health = agent.Health;
             }
 
@@ -203,7 +258,7 @@ namespace WalkerSim
             agent.CurrentState = Agent.State.Active;
             agent.Health = spawnedAgent.Health;
 
-            Logging.Out("Agent spawned at {0}, {1}, {2}, entity id {3}", worldPos.x, worldPos.y, worldPos.z, spawnedAgent.entityId);
+            Logging.Debug("Agent spawned at {0}, {1}, {2}, entity id {3}", worldPos.x, worldPos.y, worldPos.z, spawnedAgent.entityId);
 
             return spawnedAgent.entityId;
         }
