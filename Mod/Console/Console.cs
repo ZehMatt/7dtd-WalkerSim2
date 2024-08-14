3f325e1c353051b890efa4460c899f42e7804f97
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-08-14 23:45:21 +0300

    Add commands to pause/resume simulation, add better help text

--- Mod/Console/Console.cs
+++ Mod/Console/Console.cs
@@ -4,17 +4,51 @@ namespace WalkerSim.Console
 {
     public class CommandWalkerSim : ConsoleCmdAbstract
     {
+        private string GetHelpText()
+        {
+            return "Usage: walkersim <command>.\n" +
+                "List of commands:\n" +
+                " - show\n" +
+                "   Opens a new window with the simulation rendering, works only in singleplayer games.\n" +
+                " - pause\n" +
+                "   Pauses the simulation which also stops spawning and despawning.\n" +
+                " - resume\n" +
+                "   Resumes the simulation and also the spawning/despawning.";
+        }
+
+        private void ShowHelpText(string error)
+        {
+            if (error != null && error != "")
+            {
+                SdtdConsole.Instance.Output("ERROR: " + error);
+            }
+            SdtdConsole.Instance.Output(GetHelpText());
+        }
+
         public override void Execute(List<string> _params, CommandSenderInfo _senderInfo)
         {
-            if (_params.Count < 1)
-                return;
-
-            string cmd = _params[0];
-            if (cmd.ToLower() == "show")
+            if (_params.Count == 0)
             {
-                Log.Out("Showing the window");
+                ShowHelpText("No command was specified");
+                return;
+            }
 
+            string cmd = _params[0].ToLower();
+            if (cmd == "show")
+            {
                 LocalPlayerUI.primaryUI.windowManager.Open("walkersim", true);
+            }
+            else if (cmd == "pause")
+            {
+                Simulation.Instance.SetPaused(true);
+            }
+            else if (cmd == "resume")
+            {
+                Simulation.Instance.SetPaused(false);
+            }
+            else
+            {
+                ShowHelpText("Unknown command: " + cmd);
             }
         }
 
@@ -25,7 +59,7 @@ namespace WalkerSim.Console
 
         public override string getDescription()
         {
-            return "";
+            return GetHelpText();
         }
     }
 }
