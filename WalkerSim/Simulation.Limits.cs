9260e66f0cef2a7856ba4bf926f1310a0ff40d6b
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2024-10-11 13:41:20 +0300

    #4: Raise the density limit to 4000 and max agents to 1000000

--- WalkerSim/Simulation.Limits.cs
+++ WalkerSim/Simulation.Limits.cs
@@ -6,8 +6,8 @@
         {
             public const uint TicksToAdvanceOnStartup = 400;
             public const int MinDensity = 1;
-            public const int MaxDensity = 400;
-            public const int MaxAgents = 40000;
+            public const int MaxDensity = 4_000;
+            public const int MaxAgents = 1_000_000;
             public const int MaxQuerySize = 128;
             public const double SpawnDespawnDelay = 0.03;
             public const float SoundDecayRate = 20.0f;
