b4530b8d13329ff6499803ec1b85faa3493d9c73
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2025-05-01 16:31:20 +0300

    Increase the ticks for fast advancing new simulations

--- WalkerSim/Simulation.Limits.cs
+++ WalkerSim/Simulation.Limits.cs
@@ -4,7 +4,7 @@ namespace WalkerSim
     {
         public static class Limits
         {
-            public const uint TicksToAdvanceOnStartup = 4000;
+            public const uint TicksToAdvanceOnStartup = 2000;
             public const int MinDensity = 1;
             public const int MaxDensity = 4_000;
             public const int MaxAgents = 1_000_000;
