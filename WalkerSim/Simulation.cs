d3cc51d06a9b54af93734d3c2e0ad5f50c162f6e
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2026-01-25 20:24:47 +0200

    Move TimeMeasurement into its own file

--- WalkerSim/Simulation.cs
+++ WalkerSim/Simulation.cs
@@ -1,58 +1,8 @@
 using System;
-using System.Diagnostics;
 using System.Threading;
 
 namespace WalkerSim
 {
-    internal class TimeMeasurement
-    {
-        private Stopwatch _sw = new Stopwatch();
-        private float[] _samples = new float[64];
-
-        private int _index = 0;
-        private int _count = 0;
-
-        public void Add(float time)
-        {
-            _samples[_index % _samples.Length] = time;
-            _index++;
-            _count = System.Math.Min(_count + 1, _samples.Length);
-        }
-
-        public void Reset()
-        {
-            _index = 0;
-            _count = 0;
-        }
-
-        public void Restart()
-        {
-            _sw.Restart();
-        }
-
-        public float Capture()
-        {
-            var elapsed = (float)_sw.Elapsed.TotalSeconds;
-            Add(elapsed);
-            return elapsed;
-        }
-
-        public float Average
-        {
-            get
-            {
-                if (_count == 0)
-                    return 0.0f;
-                float sum = 0.0f;
-                for (int i = 0; i < _count; i++)
-                {
-                    sum += _samples[i];
-                }
-                return sum / _count;
-            }
-        }
-    }
-
     internal partial class Simulation
     {
         public static Simulation Instance = new Simulation();
