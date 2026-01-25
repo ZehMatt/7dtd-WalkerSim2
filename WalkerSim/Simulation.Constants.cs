32d59bac3c59adab41a36c2c7345b9a38f16863e
ζeh Matt <5415177+ZehMatt@users.noreply.github.com>
2026-01-25 20:26:54 +0200

    Bump up save version, remove unused constant

--- WalkerSim/Simulation.Constants.cs
+++ WalkerSim/Simulation.Constants.cs
@@ -10,15 +10,14 @@ namespace WalkerSim
             // Version 8: New configuration SoundDistanceScale.
             // Version 9: Might have saved an empty state, skip loading the old one.
             // Version 11: State for CityVisitor.
-            public const uint SaveVersion = 11;
+            // Version 12: Change type of Ticks to uint.
+            public const uint SaveVersion = 12;
 
             public const uint MaxUpdateCountPerTick = 2000;
 
             public const uint TicksPerSecond = 40;
             public const float TickRate = 1f / TicksPerSecond;
             public const uint TickRateMs = 1000 / TicksPerSecond;
-
-            public const float WalkSpeed = 1.1f; // 1.1 meters per second
         }
     }
 }
